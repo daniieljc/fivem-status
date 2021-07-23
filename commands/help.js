8309a621c3cc983f2e889d21384faf214add163f
daniieljc <danieljcpya354@gmail.com>
2021-07-23 18:24:42 +0200

    change command status for cstatus

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
             .setColor(0xFFA76D)
             .setFooter("Bot developer: hostemy.com", client.user.avatarURL())
             .setTimestamp()
-            .addField(".status", "Check the status of FiveM services.", true)
+            .addField("cstatus", "Check the status of FiveM services.", true)
             .addField("Auto Check", "The client's status will also appear in the bot's activity.", true);
 
         return embedDatos
